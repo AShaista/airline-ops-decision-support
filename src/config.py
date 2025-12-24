f89139c51e6e4d08592c56e115db64e86fc0cdff
Shaista <abroshaistakhan@gmail.com>
2025-12-23 23:31:05 -0600

    Fix RAW_DATA_PATH

--- src/config.py
+++ src/config.py
@@ -2,8 +2,7 @@ from pathlib import Path
 
 # Project root assumed as repository root
 REPO_ROOT = Path(__file__).resolve().parents[1]
-
-RAW_DATA_PATH = REPO_ROOT / "data" / "raw" / "airline_delay_sample.csv"
+RAW_DATA_PATH = REPO_ROOT / "data" / "raw" / "Airline_Delay.csv"
 PROCESSED_DIR = REPO_ROOT / "data" / "processed"
 PROCESSED_DIR.mkdir(parents=True, exist_ok=True)
 
